36447b7e3a8b58accda855c0bbb74e496b0952b6
lampamazaza <igklmp212@gmail.com>
2022-10-24 14:30:45 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="m-auto mt-8 p-8 cursor-pointer  max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white whitespace-nowrap">
+        react-vite-tailwind-boilerplate
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Sus Amogus sers!
+      </p>
+    </div>
+  );
+}
